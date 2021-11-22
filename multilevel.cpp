77076866e588bdedc758bda13adda54b2dba7587
Ramyasrisanaboina <84370631+Ramyasrisanaboina@users.noreply.github.com>
2021-11-22 12:26:34 +0530

    Add files via upload

---
+++ multilevel.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	protected:
+		string name;
+		int pinno;
+		void getst()
+		{
+			cout<<"enter name and pin no";
+			cin>>name>>pinno;
+		}
+};
+class marks:public student
+{
+	protected:
+		int m1,m2,m3,m4,m5,m6;
+		string branch;
+		void getm()
+		{
+			getst();
+			cout<<"enter 6 subjects marks";
+			cin>>m1>>m2>>m3>>m4>>m5>>m6;
+			cout<<"enter branch";
+			cin>>branch;
+			
+		}
+		void showm()
+		{
+		  cout<<"name="<<name<<endl;
+		  cout<<"pinno="<<pinno<<endl;
+		  cout<<"branch="<<branch<<endl;
+		}
+};
+class percentage:public marks
+{
+	private:
+		float per;
+		public:
+			void result()
+			{
+				getm();
+				per=(float)(m1+m2+m3+m4+m5+m6)/6;
+				showm();
+				cout<<"percentage="<<per;
+			}
+	
+};
+int main()
+{
+	percentage p;
+	p.result();
+	return 0;
+}
